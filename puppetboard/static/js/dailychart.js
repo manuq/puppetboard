def569ac3dca0c263d1b3572a64babdface86cbf
Manuel Quiñones <manuel.por.aca@gmail.com>
2016-10-25 11:34:28 -0300

    Fixup

--- puppetboard/static/js/dailychart.js
+++ puppetboard/static/js/dailychart.js
@@ -1,8 +1,13 @@
 jQuery(function ($) {
   function generateChart(el) {
-    console.log(el);
+    var url = "/daily_reports_chart.json";
     var certname = $(el).attr('data-certname');
+    if (typeof certname !== typeof undefined && certname !== false) {
+        url = url + "?certname=" + certname;
+    }
+    console.log($(el).attr('id'));
     console.log(certname);
+    console.log(url);
   }
   generateChart($("#dailyReportsChart"));
 });
