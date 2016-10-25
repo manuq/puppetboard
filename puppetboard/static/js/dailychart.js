e3ba2f89b1dc3b07f8b75df0582c7b86608140f8
Manuel Quiñones <manuel.por.aca@gmail.com>
2016-10-25 11:40:55 -0300

    Fixup

--- puppetboard/static/js/dailychart.js
+++ puppetboard/static/js/dailychart.js
@@ -3,11 +3,37 @@ jQuery(function ($) {
     var url = "/daily_reports_chart.json";
     var certname = $(el).attr('data-certname');
     if (typeof certname !== typeof undefined && certname !== false) {
-        url = url + "?certname=" + certname;
+      url = url + "?certname=" + certname;
     }
-    console.log($(el).attr('id'));
-    console.log(certname);
-    console.log(url);
+    d3.json(url, function(data) {
+      var chart = c3.generate({
+        bindto: '#dailyReportsChart',
+        data: {
+          type: 'bar',
+          json: data['result'],
+          keys: {
+            x: 'day', // it's possible to specify 'x' when category axis
+            value: ['failed', 'changed', 'unchanged'],
+          },
+          groups: [
+            ['failed', 'changed', 'unchanged']
+          ],
+          colors: {  // Must match CSS colors
+            'failed':'#AA4643',
+            'changed':'#4572A7',
+            'unchanged':'#89A54E',
+          }
+        },
+        size: {
+          height: 160
+        },
+        axis: {
+          x: {
+            type: 'category'
+          }
+        }
+      });
+    });
   }
   generateChart($("#dailyReportsChart"));
 });
